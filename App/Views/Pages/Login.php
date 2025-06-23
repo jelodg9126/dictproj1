b0c64dd8f30bbfba287e9bb29ed59805bad5693d
Markdg <markdg977@gmail.com>
2025-06-23 15:13:28 +0800

    modify login page

--- App/Views/Pages/Login.php
+++ App/Views/Pages/Login.php
@@ -8,19 +8,21 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="../../dictproj1/App/Model/dbLogin.php" method="POST">
+    <form action="../../Model/dbLogin.php" method="POST">
         
-             <div class="login-container h-screen w-full flex items-center justify-center">
-           <div class="login-card p-12 pr-6 border border-gray-400 rounded-sm ">
-            <div class="login-header-wrapper flex items-center">
-                <h1 class="text-3xl font-bold">Log in</h1> 
-                <img src="dictproj1/public/assets/images/dictStandard.png" class="h-38 min-w-auto object-cover" alt="dict logo"/>
-            </div>
-            <label>Username</label>
-             <input type="text" name="uNameLogin"class="border"/>
-            <label>Password</label>
-                 <input type="password" name="pNameLogin" class="border"/>
-              <button class="btn btn-wide mt-4 bg-blue-900 text-white">Submit</button>
+        <div class="login-container h-screen bg-gray-100 w-full flex items-center justify-center">
+           <div class="login-card p-10 px-30 bg-white py-20 flex flex-col border border-gray-400 rounded-sm shadow-xl">
+                
+
+                <img src="/dictproj1/public/assets/images/dictStandard2.png" class="mx-auto max-w-auto w-full h-36 min-w-0 object-cover" alt="dict logo"/>
+                <h1 class="text-4xl font-bold text-blue-900 text-center mb-6 mt-8">Log In</h1> 
+               
+          
+          <div class="flex flex-col gap-3">
+             <input type="text" name="uNameLogin" class="border border-gray-400 rounded-xl p-2 pl-3 w-[350px] mx-auto" placeholder="Username"/>
+             <input type="password" name="pNameLogin" class="border border-gray-400 rounded-xl p-2 pl-3 w-[350px] mx-auto" placeholder="Password"/>
+              <button class="btn bg-blue-900 font-bold text-white p-2 rounded-xl cursor-pointer max-w-xl w-full mx-auto">Submit</button>
+</div>
            </div>
         </div>
     </form>
