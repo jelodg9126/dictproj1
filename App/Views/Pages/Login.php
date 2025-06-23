112651aff2686e92cf369e13375c016b87f1cc37
TumbleMane <ingco884@gmail.com>
2025-06-23 14:20:35 +0800

    signout login page redirect fixed

--- App/Views/Pages/Login.php
+++ App/Views/Pages/Login.php
@@ -8,13 +8,14 @@
     <title>Document</title>
 </head>
 <body>
+
     <form action="../../Model/dbLogin.php" method="POST">
         
              <div class="login-container h-screen w-full flex items-center justify-center">
            <div class="login-card p-12 pr-6 border border-gray-400 rounded-sm ">
             <div class="login-header-wrapper flex items-center">
                 <h1 class="text-3xl font-bold">Log in</h1> 
-                <img src="../public/assets/images/dictStandard.png" class="h-38 min-w-auto object-cover" alt="dict logo"/>
+                <img src="../../../public/assets/images/dictStandard.png" class="h-38 min-w-auto object-cover" alt="dict logo"/>
             </div>
             <label>Username</label>
              <input type="text" name="uNameLogin"class="border"/>
