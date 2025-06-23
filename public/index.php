f05a684362ae496f005b020bf2a1de0b70fd3619
Umaruuuuuuu <reignreyes10@gmail.com>
2025-06-23 10:35:29 +0800

    resolved merge conflict

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ if ($page === 'dashboard') {
     include __DIR__ . '/../App/Views/Pages/Dashboard.php';
 } elseif ($page === 'documents') {
     include __DIR__ . '/../App/Views/Pages/Documents.php';
+} elseif ($page === 'logout') {
+    include __DIR__ . '/../App/Views/Pages/Login.php';
 } else {
     // 404 or default
 }
